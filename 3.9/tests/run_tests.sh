6fe90c8943389f5fa7ff11e8b4b72b5622790a00
Christophe Labouisse <christophe@labouisse.org>
2022-02-04 09:14:56 +0100

    Add automated tests

---
+++ 3.9/tests/run_tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+MY_DIR=$(dirname $0)
+
+cd $MY_DIR
+
+echo "Checking python version"
+python --version | grep -q '^Python 3.9'
+
+echo "Installing Pandas"
+pip install Pandas
+
+echo "Read CSV file & check shape"
+python -c "import pandas as pd; df = pd.read_csv('data.csv'); assert df.shape == (3, 3), f'Bad shape: {df.shape} vs (3, 3)'"
