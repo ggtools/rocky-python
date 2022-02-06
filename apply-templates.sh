292a2f28ee63dc822f43d22b3c3bbfda1e2cda02
Christophe Labouisse <christophe@labouisse.org>
2022-02-07 00:01:31 +0100

    Add bash script to update all versions

---
+++ apply-templates.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+VERSIONS_URL=${VERSIONS_URL:-https://raw.githubusercontent.com/docker-library/python/master/versions.json}
+
+VERSIONS_JSON=$(mktemp -t versions.XXXXXXXX.json)
+
+curl -s -o $VERSIONS_JSON $VERSIONS_URL
+
+for version in $(jq -r 'keys[]' $VERSIONS_JSON)
+do
+    if [ -d $version ]
+    then
+        echo "Generating templates for $version"
+        PY_BRANCH="$version" gomplate -d versions=$VERSIONS_JSON -t ./templates/support.t --input-dir=templates --output-dir=$version
+    else
+        echo "Skipping version $version"
+    fi
+done
+
+# rm -f $VERSIONS_JSON
