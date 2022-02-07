e98e84d5b0f4bbac1a46450d81cc61151b5bfd53
Christophe Labouisse <christophe@labouisse.org>
2022-02-07 09:50:01 +0100

    Update versions from upstream

--- 3.10/tests/run_tests.sh
+++ 3.10/tests/run_tests.sh
@@ -5,7 +5,7 @@ MY_DIR=$(dirname $0)
 cd $MY_DIR
 
 echo "Checking python version"
-python --version | grep -q '^Python 3.10'
+python --version | grep -q '^Python 3.10.2'
 
 echo "Installing Pandas"
 pip install Pandas
