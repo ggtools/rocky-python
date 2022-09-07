5502a3d64e442b11f91f4ed5d6e79084d099bc2c
ggtools <ggtools@users.noreply.github.com>
2022-09-07 05:47:52 +0000

    [create-pull-request] Apply templates from official images

--- 3.10/tests/run_tests.sh
+++ 3.10/tests/run_tests.sh
@@ -5,7 +5,7 @@ MY_DIR=$(dirname $0)
 cd $MY_DIR
 
 echo "Checking python version"
-python --version | grep -q '^Python 3.10.6'
+python --version | grep -q '^Python 3.10.7'
 
 echo "Installing Pandas"
 pip install Pandas
